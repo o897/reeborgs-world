c401a0dd3c034537e8ab5f97ae5a398928f2608a
o897 <orapelengm239@gmail.com>
2023-03-24 04:37:07 +0200

    reeborgs world hurdle challenge

---
+++ main.py
@@ -0,0 +1,28 @@
+move()
+
+def turn_right() :
+        turn_left()
+        turn_left()
+        turn_left()
+
+def jump() :
+    for l in range(1,7) :  
+        for j in range (0,1) :
+            turn_left()
+            move()
+            turn_right()
+            move()
+
+        for d in range (0,1) :
+            turn_right()
+            move()
+            turn_left()
+        
+        if l == 6 :
+            done()
+            
+        move()
+          
+jump()
+
+    
